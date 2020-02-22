fea087dbd882f4a8bc2fb3e3fd17d137ed5f95fc
salwaelmohandes <60805227+salwaelmohandes@users.noreply.github.com>
2020-02-21 22:25:58 -0500

    the search function

--- js/script.js
+++ js/script.js
@@ -2,18 +2,49 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+// create and append a search bar 
+// create a new student list variable based on the search matches and the Html  
+// use it as an argument when calling the existing functions
+// add a class name and use it to select the matching Elements
+
+const search = document.createElement("div");
+   search.classList.add("student-search")
+const pageHeader = document.getElementsByClassName("page-header");
+const input = document.createElement("input");
+   input.setAttribute("placeholder","search for student...");
+const button = document.createElement("button");
+   button.textContent = "search";
+      pageHeader[0].appendChild(search).appendChild(input);
+      search.appendChild(button);
+const details = document.getElementsByClassName("student-details");
+
+   button.addEventListener('click', (event) => {
+   event.preventDefault();
+   
+const newList = [];
+   for (let i=0; i<details.length; i+=1){
+      studentList[i].style.display= "none";
+      if (input.value.length !== 0 && 
+         details[i].textContent.toLowerCase().includes(input.value.toLowerCase())) {
+         newList.push(studentList[i]);
+      }
+   }
+   showPage(newList, 1); 
+});
+
    
 // Add global variable that store the DOM elements I will 
 // need to reference and manipulate. 
 
 const studentList = document.getElementsByClassName("student-item");
- const pageItems = 10;
+const pageItems = 10;
 
 // Create the `showPage` function with two parameter to hide all of 
 // the items in the student list except for the ten I want to show.
 
-const showPage = (studentList,page)=> {
-
+const showPage = (students,page)=> {
+   console.log(students);
+       
 // Create two variables inside the ShowPage function to store the start 
 // index and the end index of the list items to be displayed on the page. 
 // To make this function dynamic and work with a list of any length. 
@@ -21,17 +52,19 @@ const showPage = (studentList,page)=> {
    let firstPage = (page * pageItems)-pageItems;
    let lastPage = page * pageItems;
 
+   console.log(firstPage, lastPage);
+
 // Loop over the list parameter.
-   for (let i=0; i < studentList.length; i+=1){
+   for (let i=0; i < students.length; i+=1){
 
 // Display studentlist with an index that is greater than or equal to 
 // the start index variable and less than the end index variable to show 
 // the first page only not the whole student list.
 
        if (i>=firstPage && i<lastPage){ 
-         studentList[i].style.display = '';
+         students[i].style.display = '';
       }else{
-         studentList[i].style.display = 'none';
+         students[i].style.display = 'none';
       }
    }
 };
@@ -41,8 +74,8 @@ showPage(studentList,1);
 // Create the `appendPageLinks function` 
 // to create pagination buttons and add functionality to it.
 
-const appendPageLinks = (studentList) => {
-   let numOfPages = Math.ceil(studentList.length/pageItems);
+const appendPageLinks = (students) => {
+   let numOfPages = Math.ceil(students.length/pageItems);
 // create a div and add a class to it.
    const div = document.createElement("div");
    div.classList.add ("pagination");
@@ -58,6 +91,7 @@ const appendPageLinks = (studentList) => {
    for (let i=0; i<numOfPages; i+=1){
       let li = document.createElement("li");
       let a = document.createElement ("a");
+      a.setAttribute("href","#")
       ul.appendChild(li).appendChild(a);
 // add +1 to i because the page numbers should start with 1 not 0.
       a.textContent = i+1;
@@ -69,28 +103,27 @@ const appendPageLinks = (studentList) => {
    const aLinks = ul.querySelectorAll('a');
 
 // Add event listener to each 'a' link
-   for(let i=0; i<aLinks.length; i++) {
+   for(let i=0; i<aLinks.length; i+=1) {
       aLinks[i].addEventListener('click', (e) => {
 
 // remove active from all links
-         for (let j=0; j<aLinks.length; j+=1) {
+      for (let j=0; j<aLinks.length; j+=1) {
             aLinks[j].classList.remove("active");
-         }
+         } 
 // add back "active" to clicked link
          e.target.classList.add("active");
 
 // call the showPage function.
-         showPage(studentList,i+1);
+         showPage(students,i+1);
       });
    }
 // add the ul to the div and the div to the page.
    page[0].appendChild(div).appendChild(ul);
+   
 };
 
 // Call the appendPageLinks function.
-      appendPageLinks(studentList);
+// appendPageLinks(studentList);
+appendPageLinks(studentList);
 
    
-  
-
-
